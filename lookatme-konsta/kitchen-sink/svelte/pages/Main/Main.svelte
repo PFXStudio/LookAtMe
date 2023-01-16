9f875411eb02a9a072d918f733c8eb304e0f04ba
pfxstudio21 <pfxstudio21@gmail.com>
2023-01-16 23:41:23 +0900

    update cards

--- lookatme-konsta/kitchen-sink/svelte/pages/Main/Main.svelte
+++ lookatme-konsta/kitchen-sink/svelte/pages/Main/Main.svelte
@@ -29,25 +29,25 @@
     <Tabbar labels icons class="left-0 bottom-0 fixed">
       <TabbarLink active label="Looker">
         <Icon slot="icon">
-          <EnvelopeFill slot="ios" class="w-7 h-7" />
+          <LookAtMeIcon slot="ios" class="w-7 h-7" />
           <LookAtMeIcon slot="material" class="w-6 h-6" />
         </Icon>
       </TabbarLink>
       <TabbarLink label="Match">
         <Icon slot="icon" >
-          <Calendar slot="ios" class="w-7 h-7" />
+          <MatchIcon slot="ios" class="w-7 h-7" />
           <MatchIcon slot="material" class="w-6 h-6" />
         </Icon>
       </TabbarLink>
       <TabbarLink label="Notice">
         <Icon slot="icon" >
-          <CloudUploadFill slot="ios" class="w-7 h-7" />
+          <NoticeIcon slot="ios" class="w-7 h-7" />
           <NoticeIcon slot="material" class="w-6 h-6" />
         </Icon>
       </TabbarLink>
       <TabbarLink label="Setup">
         <Icon slot="icon" >
-          <CloudUploadFill slot="ios" class="w-7 h-7" />
+          <SetupIcon slot="ios" class="w-7 h-7" />
           <SetupIcon slot="material" class="w-6 h-6" />
         </Icon>
       </TabbarLink>
--- lookatme-konsta/kitchen-sink/svelte/pages/Scrap/Scrap.svelte
+++ lookatme-konsta/kitchen-sink/svelte/pages/Scrap/Scrap.svelte
@@ -6,11 +6,13 @@
       Button,
       BlockTitle,
     } from 'konsta/svelte';
+    let cards = [1, 2, 3]
   </script>
   
-  <Page>
+  <div style="{'margin-bottom:4rem'}">
     <BlockTitle withBlock={false}>Simple Cards</BlockTitle>
     <div class="lg:grid lg:grid-cols-2">
+      {#each cards as card}
       <Card>
         <div
           slot="header"
@@ -33,50 +35,7 @@
           </div>
         </svelte:fragment>
       </Card>
-      <Card>
-        <div
-          slot="header"
-          class="ios:-mx-4 ios:-mt-4 h-48 p-4 flex items-end text-white ios:font-bold bg-cover bg-center material:rounded-xl mb-4 material:text-[22px]"
-          style="background-image: url(https://cdn.framework7.io/placeholder/people-1000x600-6.jpg)"
-        >
-          Lorem Ipsum
-        </div>
-        <div class="text-gray-500 mb-3">Posted on January 21, 2021</div>
-        <p>
-          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies
-          efficitur vitae non felis. Phasellus quis nibh hendrerit...
-        </p>
-        <svelte:fragment slot="footer">
-          <div class="flex justify-end material:hidden">
-            <Link>정보 가져오기</Link>
-          </div>
-          <div class="flex justify-end ios:hidden space-x-2">
-            <Button rounded inline outline>정보 가져오기</Button>
-          </div>
-        </svelte:fragment>
-      </Card>
-      <Card>
-        <div
-          slot="header"
-          class="ios:-mx-4 ios:-mt-4 h-48 p-4 flex items-end text-white ios:font-bold bg-cover bg-center material:rounded-xl mb-4 material:text-[22px]"
-          style="background-image: url(https://cdn.framework7.io/placeholder/people-1000x600-6.jpg)"
-        >
-          Lorem Ipsum
-        </div>
-        <div class="text-gray-500 mb-3">Posted on January 21, 2021</div>
-        <p>
-          Quisque eget vestibulum nulla. Quisque quis dui quis ex ultricies
-          efficitur vitae non felis. Phasellus quis nibh hendrerit...
-        </p>
-        <svelte:fragment slot="footer">
-          <div class="flex justify-end material:hidden">
-            <Link>정보 가져오기</Link>
-          </div>
-          <div class="flex justify-end ios:hidden space-x-2">
-            <Button rounded inline outline>정보 가져오기</Button>
-          </div>
-        </svelte:fragment>
-      </Card>
+    {/each}
     </div>
-  </Page>
+  </div>
   
